c39cb5066b3e0bf2e11054f7b1a1af06478dbc1d
cryobyt3 <87677766+cryobyt3@users.noreply.github.com>
2021-07-21 20:14:54 +0300

    Updated everything

--- Launcher/Launcher/Launcher.cs
+++ Launcher/Launcher/Launcher.cs
@@ -22,55 +22,59 @@ namespace Launcher
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (File.Exists("auto.exe"))
+            if (!Directory.Exists("Danker"))
             {
-                File.Delete("auto.exe");
+                Directory.CreateDirectory("Danker");
+            }
+            if (File.Exists("Danker\\auto.exe"))
+            {
+                File.Delete("Danker\\auto.exe");
             }
             WebClient auto = new WebClient();
             auto.DownloadProgressChanged += new DownloadProgressChangedEventHandler(autoProgressBar);
-            auto.DownloadFileAsync(new Uri("http://95.179.167.23/auto.exe"), "auto.exe");
-            if (File.Exists("Danker.exe"))
+            auto.DownloadFileAsync(new Uri("http://95.179.167.23/auto.exe"), "Danker\\auto.exe");
+            if (File.Exists("Danker\\Danker.exe"))
             {
-                File.Delete("Danker.exe");
+                File.Delete("Danker\\Danker.exe");
             }
             WebClient danker = new WebClient();
             danker.DownloadProgressChanged += new DownloadProgressChangedEventHandler(dankerProgressBar);
-            danker.DownloadFileAsync(new Uri("http://95.179.167.23/Danker.exe"), "Danker.exe");
-            if (File.Exists("Guna.UI2.dll"))
+            danker.DownloadFileAsync(new Uri("http://95.179.167.23/Danker.exe"), "Danker\\Danker.exe");
+            if (File.Exists("Danker\\Guna.UI2.dll"))
             {
-                File.Delete("Guna.UI2.dll");
+                File.Delete("Danker\\Guna.UI2.dll");
             }
             WebClient gunaui = new WebClient();
             gunaui.DownloadProgressChanged += new DownloadProgressChangedEventHandler(gunauiProgressBar);
-            gunaui.DownloadFileAsync(new Uri("http://95.179.167.23/Guna.UI2.dll"), "Guna.UI2.dll");
-            if (File.Exists("INIFileParser.dll"))
+            gunaui.DownloadFileAsync(new Uri("http://95.179.167.23/Guna.UI2.dll"), "Danker\\Guna.UI2.dll");
+            if (File.Exists("Danker\\INIFileParser.dll"))
             {
-                File.Delete("INIFileParser.dll");
+                File.Delete("Danker\\INIFileParser.dll");
             }
             WebClient iniparser = new WebClient();
             iniparser.DownloadProgressChanged += new DownloadProgressChangedEventHandler(iniparserProgressBar);
-            iniparser.DownloadFileAsync(new Uri("http://95.179.167.23/INIFileParser.dll"), "INIFileParser.dll");
-            if (!File.Exists("configs\\config.ini"))
+            iniparser.DownloadFileAsync(new Uri("http://95.179.167.23/INIFileParser.dll"), "Danker\\INIFileParser.dll");
+            if (!File.Exists("configs\\default.ini"))
             {
-                if (!Directory.Exists("configs"))
+                if (!Directory.Exists("Danker\\configs"))
                 {
-                    Directory.CreateDirectory("configs");
+                    Directory.CreateDirectory("Danker\\configs");
                 }
                 WebClient con = new WebClient();
                 con.DownloadProgressChanged += new DownloadProgressChangedEventHandler(conProgressBar);
-                con.DownloadFileAsync(new Uri("http://95.179.167.23/config.ini"), "configs\\config.ini");
+                con.DownloadFileAsync(new Uri("http://95.179.167.23/default.ini"), "Danker\\configs\\default.ini");
             }
             else
             {
                 guna2ProgressBar3.Value = 100;
             }
+            if (File.Exists("Danker\\configs\\_.cfg"))
+            {
+                File.Delete("Danker\\configs\\_.cfg");
+            }
             WebClient config = new WebClient();
             config.DownloadProgressChanged += new DownloadProgressChangedEventHandler(configProgressBar);
-            config.DownloadFileAsync(new Uri("http://95.179.167.23/_.cfg"), "_.cfg");
-            if (File.Exists("_.cfg"))
-            {
-                File.Delete("_.cfg");
-            }
+            config.DownloadFileAsync(new Uri("http://95.179.167.23/_.cfg"), "Danker\\configs\\_.cfg");
         }
 
         public void autoProgressBar(Object sender, DownloadProgressChangedEventArgs e)
